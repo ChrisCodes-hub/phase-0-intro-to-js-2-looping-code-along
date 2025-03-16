ed21ea0c9092ec1fc58ac941adbbc6df2e9ad530
christopherchege <chrisshege35@gmail.com>
2025-03-16 03:39:42 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+function countDown(){
+   // let writeCards = []; 
+    for(let num=10;num >=0;num--){
+        console.log(num)
+     //   let writeCards = `${num} Thank you!`; 
+     //   writeCards.push(message);  
+   }
+  // console.log(messages);
+}
+
+
+
+// New writeCards function to fix the error
+function writeCards(names, event) {
+    let messages = [];
+
+    for (let i = 0; i <3; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return messages;
+}
